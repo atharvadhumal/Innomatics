9bc203eafb5ad689410b6d29e2440d9a0fac24a1
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-02-02 16:35:16 +0530

    made expense tracker

---
+++ expense tracker/main.js
@@ -0,0 +1,91 @@
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select')
+const amountInput = document.getElementById('amount-input')
+const dateInput = document.getElementById('date-input')
+const addBtn = document.getElementById('add-button')
+const expensesTable = document.getElementById('expenses-table')
+const totalAmountCell = document.getElementById('total-amount')
+
+addBtn.addEventListener('click', ()=> {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+
+    if(category === '') {
+        alert("Please enter a Category");
+        return;
+    }
+
+    if(isNaN(amount) || amount <= 0){
+        alert("Please enter valid amount")
+        return;
+    }
+    
+    if(date === '') {
+        alert('Please select a date')
+        return;
+    }
+
+    expenses.push({category, amount, date})
+
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTable.insertRow();
+
+    const  categoryCell = newRow.insertCell()
+    const  amountCell = newRow.insertCell()
+    const  dateCell = newRow.insertCell()
+    const deleteCell = newRow.insertCell()
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expenses.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTable.removeChild(newRow);
+
+    });
+
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn)
+})
+
+for(const expense of expenses) {
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTable.insertRow()
+    const  categoryCell = newRow.insertCell()
+    const  amountCell = newRow.insertCell()
+    const  dateCell = newRow.insertCell()
+    const deleteCell = newRow.insertCell()
+    const deteteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expenses), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTable.removeChild(newRow);
+
+    });
+    
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
+
+}
